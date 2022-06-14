4d14098b61460efd63fbeee0f22bd4185428c534
Ashish Kapoor <swiftobjc@gmail.com>
2022-06-14 13:49:28 +0530

    get all posts

--- api/routers/post.py
+++ api/routers/post.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from routers.schemas import PostBase, PostDisplay
 from db.database import get_db
 from db import db_post
+from typing import List
 
 router = APIRouter(
     prefix='/post',
@@ -21,3 +22,8 @@ def create_post(request: PostBase, db: Session = Depends(get_db)):
             detail="Parameter image_url_type can only take values 'absolute' or 'relative'."
         )
     return db_post.create_post(db, request)
+
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
