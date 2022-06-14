38a5db0b502ca7b7fda6f2fbf312d6443837aaf1
Ashish Kapoor <swiftobjc@gmail.com>
2022-06-14 21:38:26 +0530

    fix auth

--- api/auth/authentication.py
+++ api/auth/authentication.py
@@ -1,7 +1,7 @@
 from os import access
 from venv import create
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.param_functions import OAuth2PasswordRequestForm
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from db.database import get_db
 from sqlalchemy.orm.session import Session
 from db.models import DbUser
