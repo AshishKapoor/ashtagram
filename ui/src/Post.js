ed11b98b2e9d5aeea23ead9b594b4bf773c2cb1a
Ashish Kapoor <swiftobjc@gmail.com>
2022-06-16 11:12:57 +0530

    delete and comment feature for posts added

--- ui/src/Post.js
+++ ui/src/Post.js
@@ -1,24 +1,98 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Post.css";
-import { BASE_URL } from "./App";
 import { Avatar, Button } from "@material-ui/core";
 
-function Post({ post }) {
-  const { image_url, image_url_type } = post;
+const BASE_URL = "http://localhost:8000/";
+
+function Post({ post, authToken, authTokenType, username }) {
   const [imageUrl, setImageUrl] = useState("");
   const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
-    if (image_url_type === "absolute") {
-      setImageUrl(image_url);
+    if (post.image_url_type === "absolute") {
+      setImageUrl(post.image_url);
     } else {
-      setImageUrl(BASE_URL + image_url);
+      setImageUrl(BASE_URL + post.image_url);
     }
-  }, [image_url, image_url_type]);
+  }, []);
 
   useEffect(() => {
     setComments(post.comments);
-  }, [post.comments]);
+  }, []);
+
+  const handleDelete = (event) => {
+    event?.preventDefault();
+
+    const requestOptions = {
+      method: "GET",
+      headers: new Headers({
+        Authorization: authTokenType + " " + authToken,
+      }),
+    };
+
+    fetch(BASE_URL + "post/delete/" + post.id, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          window.location.reload();
+        }
+        throw response;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const postComment = (event) => {
+    event?.preventDefault();
+
+    const json_string = JSON.stringify({
+      username: username,
+      text: newComment,
+      post_id: post.id,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: new Headers({
+        Authorization: authTokenType + " " + authToken,
+        "Content-Type": "application/json",
+      }),
+      body: json_string,
+    };
+
+    fetch(BASE_URL + "comment", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        fetchComments();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setNewComment("");
+      });
+  };
+
+  const fetchComments = () => {
+    fetch(BASE_URL + "comment/all/" + post.id)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setComments(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="post">
@@ -26,19 +100,43 @@ function Post({ post }) {
         <Avatar alt="Ashish" src="" />
         <div className="post_header_info">
           <h3>{post.user.username}</h3>
-          <Button className="post_delete">Delete</Button>
+          <Button className="post_delete" onClick={handleDelete}>
+            Delete
+          </Button>
         </div>
       </div>
+
       <img className="post_image" src={imageUrl} alt="post" />
+
       <h4 className="post_text">{post.caption}</h4>
+
       <div className="post_comments">
         {comments.map((comment) => (
           <p>
-            <strong>{comment.username}: </strong>
-            {comment.text}
+            <strong>{comment.username}:</strong> {comment.text}
           </p>
         ))}
       </div>
+
+      {authToken && (
+        <form className="post_comment_box">
+          <input
+            className="post_input"
+            type="text"
+            placeholder="Add a comment"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <button
+            className="post_button"
+            type="submit"
+            disabled={!newComment}
+            onClick={postComment}
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
