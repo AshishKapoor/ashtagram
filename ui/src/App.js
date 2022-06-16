a9cee0d997aebde55bd61071aa0b6d578dde3abf
Ashish Kapoor <swiftobjc@gmail.com>
2022-06-16 10:39:09 +0530

    reverse the posts order

--- ui/src/App.js
+++ ui/src/App.js
@@ -15,6 +15,18 @@ function App() {
         throw response;
       })
       .then((data) => {
+        // return data.reverse();
+        const result = data.sort((a,b) => {
+          // 2022-06-15T11:18:59.014091
+          const t_a = a.timestamp.split(/[-T:]/);
+          const t_b = b.timestamp.split(/[-T:]/);
+          const date_a = new Date(Date.UTC(t_a[0], t_a[1]-1, t_a[2], t_a[3], t_a[4], t_a[5]))
+          const date_b = new Date(Date.UTC(t_b[0], t_b[1]-1, t_b[2], t_b[3], t_b[4], t_b[5]))
+          return date_b - date_a;
+        })
+        return result;
+      })
+      .then((data) => {
         setPosts(data);
       })
       .catch((error) => {
